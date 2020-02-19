85c4301624f738dd7ec36bec85e74a92874051fd
Student <student@Makerss-MacBook-Air-2.local>
2020-02-19 20:16:45 +0000

    Add the down method to enable decreasing of temperature

--- spec/thermostateSpec.js
+++ spec/thermostateSpec.js
@@ -10,8 +10,13 @@ describe('Thermostat', function() {
         expect(thermostat.getCurrentTemp()).toEqual(20);
     });
 
-    it('allows the user to increase the temperature with up', function(){
+    it('allows the user to increase the temperature with #up', function(){
         thermostat.up();
         expect(thermostat.getCurrentTemp()).toEqual(21);
+    });
+
+    it('allows the user to decrease the temperature with #decrease', function(){
+        thermostat.down();
+        expect(thermostat.getCurrentTemp()).toEqual(19);
     });
 });
--- src/thermostate.js
+++ src/thermostate.js
@@ -4,7 +4,13 @@ function Thermostat(){
 Thermostat.prototype.getCurrentTemp = function() {
     return this.temperature
     };
+
 Thermostat.prototype.up = function() {
     this.temperature += 1
 };
+
+Thermostat.prototype.down = function(){
+    this.temperature -= 1
+};
+
 };
